f9df4f7943f1abcc8215aa9f28a0bd46c36bc9d4
philippe86220 <142721115+philippe86220@users.noreply.github.com>
2026-06-01 17:05:25 +0200

    Add SPIPeripheralClass for SPI communication

--- sketch/SPIPeripheral.h
+++ sketch/SPIPeripheral.h
@@ -1,1 +1,63 @@
+#ifndef SPI_PERIPHERAL_H
+#define SPI_PERIPHERAL_H
 
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/drivers/spi.h>
+
+#ifdef CONFIG_BOARD_ARDUINO_UNO_Q
+
+#define SPI_PERIPHERAL_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_spi_slave)
+
+template <int SPI_MAX_MESSAGE>
+class SPIPeripheralClass {
+public:
+    SPIPeripheralClass() {
+        spi_cfg.frequency = 1000000;
+        spi_cfg.operation = SPI_WORD_SET(8) | SPI_OP_MODE_SLAVE;
+
+        rx.buf = rxmsg;
+        rx.len = SPI_MAX_MESSAGE;
+        rx_bufs.buffers = &rx;
+        rx_bufs.count = 1;
+
+        tx.buf = txmsg;
+        tx.len = SPI_MAX_MESSAGE;
+        tx_bufs.buffers = &tx;
+        tx_bufs.count = 1;
+    }
+
+    int begin() {
+        return device_init(spi_peripheral);
+    }
+
+    void depopulate(uint8_t &buf, size_t len) {
+        spi_transceive(spi_peripheral, &spi_cfg, &tx_bufs, &rx_bufs);
+        uint8_t* rx_bytes = static_cast<uint8_t*>(rx_bufs.buffers[0].buf);
+        buf = rx_bytes[0];
+    }
+
+    void* populate(uint8_t* buf, size_t len) {
+        return memcpy(tx.buf, buf, len);
+    }
+
+    int ready() {
+        return spi_transceive(spi_peripheral, &spi_cfg, &tx_bufs, &rx_bufs);
+    }
+
+private:
+    const struct device *const spi_peripheral = DEVICE_DT_GET(DT_BUS(SPI_PERIPHERAL_NODE));
+    struct spi_config spi_cfg;
+
+    uint8_t rxmsg[SPI_MAX_MESSAGE];
+    struct spi_buf rx;
+    struct spi_buf_set rx_bufs;
+
+    uint8_t txmsg[SPI_MAX_MESSAGE];
+    struct spi_buf tx;
+    struct spi_buf_set tx_bufs;
+};
+
+#endif
+#endif
